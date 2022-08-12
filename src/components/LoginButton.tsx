6a9d1f9ea9018c3198a92ad27931380705a4b794
Wowsaruss <russell.hayes.dev@gmail.com>
2022-08-12 09:26:42 -0700

    on hover nav

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import '../App.css'
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0()
@@ -11,14 +12,7 @@ const LoginButton = () => {
                     redirectUri: `${window.location.origin}/dashboard`,
                 })
             }
-            style={{
-                paddingBottom: 10,
-                paddingTop: 10,
-                width: '100%',
-                textDecoration: 'none',
-                color: 'black',
-                cursor: 'pointer',
-            }}
+            className="Link-inactive"
         >
             <h3 style={{ paddingLeft: 10, marginRight: 10 }}>Login</h3>
         </div>
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import '../App.css'
 
 const LogoutButton = () => {
     const { logout } = useAuth0()
@@ -7,14 +8,7 @@ const LogoutButton = () => {
     return (
         <div
             onClick={() => logout({ returnTo: window.location.origin })}
-            style={{
-                paddingBottom: 10,
-                paddingTop: 10,
-                width: '100%',
-                textDecoration: 'none',
-                color: 'black',
-                cursor: 'pointer',
-            }}
+            className="Link-inactive"
         >
             <h3 style={{ paddingLeft: 10, marginRight: 10 }}>Logout</h3>
         </div>
