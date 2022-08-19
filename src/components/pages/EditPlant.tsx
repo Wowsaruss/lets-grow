8eb877283017be2a7b76b0424463d060137c6a5e
Wowsaruss <russell.hayes.dev@gmail.com>
2022-08-19 08:45:26 -0700

    initial values bug

--- src/components/pages/EditPlant.tsx
+++ src/components/pages/EditPlant.tsx
@@ -50,30 +50,66 @@ export default function EditPlant() {
 
     const initialValues: FormikValues = isLoaded
         ? {
-              days_to_germination: plantData?.days_to_germination,
-              days_to_harvest: plantData?.days_to_harvest,
-              description: plantData?.description,
-              determinate: plantData?.determinate,
-              fall_start_indoors: plantData?.fall_start_indoors,
-              fall_start_outdoors: plantData?.fall_start_outdoors,
-              fall_transplant: plantData?.fall_transplant,
-              family: plantData?.family,
+              days_to_germination:
+                  plantData?.days_to_germination !== null
+                      ? plantData?.days_to_germination
+                      : '',
+              days_to_harvest:
+                  plantData?.days_to_harvest !== null
+                      ? plantData?.days_to_harvest
+                      : 0,
+              description:
+                  plantData?.description !== null ? plantData?.description : '',
+              determinate:
+                  plantData?.determinate !== null ? plantData?.determinate : '',
+              fall_start_indoors:
+                  plantData?.fall_start_indoors !== null
+                      ? plantData?.fall_start_indoors
+                      : '',
+              fall_start_outdoors:
+                  plantData?.fall_start_outdoors !== null
+                      ? plantData?.fall_start_outdoors
+                      : '',
+              fall_transplant:
+                  plantData?.fall_transplant !== null
+                      ? plantData?.fall_transplant
+                      : '',
+              family: plantData?.family !== null ? plantData?.family : '',
               first_name: plantData?.first_name,
-              germination_temps_f: plantData?.germination_temps_f,
-              last_day_to_plant: plantData?.last_day_to_plant,
-              light: plantData?.light,
-              perennial: plantData?.perennial,
-              plant_spacing: plantData?.plant_spacing,
-              pruning: plantData?.pruning,
-              row_spacing: plantData?.row_spacing,
+              germination_temps_f:
+                  plantData?.germination_temps_f !== null
+                      ? plantData?.germination_temps_f
+                      : '',
+              last_day_to_plant:
+                  plantData?.last_day_to_plant !== null
+                      ? plantData?.last_day_to_plant
+                      : '',
+              light: plantData?.light !== null ? plantData?.light : '',
+              perennial:
+                  plantData?.perennial !== null ? plantData?.perennial : '',
+              plant_spacing:
+                  plantData?.plant_spacing !== null
+                      ? plantData?.plant_spacing
+                      : 0,
+              pruning: plantData?.pruning !== null ? plantData?.pruning : '',
+              row_spacing:
+                  plantData?.row_spacing !== null ? plantData?.row_spacing : 0,
               second_name: plantData?.second_name,
-              seed_depth: plantData?.seed_depth,
-              soil: plantData?.soil,
-              start_indoors: plantData?.start_indoors,
-              start_outdoors: plantData?.start_outdoors,
-              transplant: plantData?.transplant,
-              type: plantData?.type,
-              water: plantData?.water,
+              seed_depth:
+                  plantData?.seed_depth !== null ? plantData?.seed_depth : '',
+              soil: plantData?.soil !== null ? plantData?.soil : '',
+              start_indoors:
+                  plantData?.start_indoors !== null
+                      ? plantData?.start_indoors
+                      : '',
+              start_outdoors:
+                  plantData?.start_outdoors !== null
+                      ? plantData?.start_outdoors
+                      : '',
+              transplant:
+                  plantData?.transplant !== null ? plantData?.transplant : '',
+              type: plantData?.type !== null ? plantData?.type : '',
+              water: plantData?.water !== null ? plantData?.water : '',
           }
         : {}
 
