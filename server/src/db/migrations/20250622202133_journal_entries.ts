c14c5c53eb0c223ae29c0d493556232a4bb940eb
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-22 13:29:51 -0700

    journal_entries

---
+++ server/src/db/migrations/20250622202133_journal_entries.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('journal_entries', (table) => {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').index();
+    table.integer('plant_id').unsigned().references('id').inTable('plants').onDelete('CASCADE').index();
+    table.date('date');
+    table.text('subject');
+    table.text('body');
+    table.date('deleted_at');
+    table.date('created_at');
+    table.date('updated_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('journal_entries');
+} 
