7927f3d18da3d6d93222d60e19232a17f437dd91
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-22 13:10:25 -0700

    plants

---
+++ server/src/db/migrations/20250622200405_plants.ts
@@ -0,0 +1,37 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('plants', (table) => {
+    table.increments('id').primary();
+    table.integer('author_id').unsigned().references('id').inTable('users').onDelete('CASCADE').index();
+    table.string('common_name');
+    table.string('scientific_name');
+    table.text('description');
+    table.integer('plant_family_id').unsigned().references('id').inTable('plant_families').onDelete('CASCADE').index();
+    table.integer('plant_type_id').unsigned().references('id').inTable('plant_types').onDelete('CASCADE').index();
+    table.integer('days_to_germination');
+    table.integer('days_to_harvest');
+    table.boolean('indeterminate');
+    table.integer('germination_temp_high');
+    table.integer('germination_temp_low');
+    table.string('light');
+    table.string('water');
+    table.string('soil');
+    table.boolean('perennial');
+    table.boolean('heirloom');
+    table.boolean('hybrid');
+    table.boolean('open_pollinated');
+    table.boolean('self_pollinated');
+    table.integer('spacing');
+    table.integer('row_spacing');
+    table.text('pruning');
+    table.string('sowing_depth');
+    table.date('deleted_at');
+    table.date('created_at');
+    table.date('updated_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('plants');
+} 
