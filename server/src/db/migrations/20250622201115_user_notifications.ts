f43cc32bcf8175ec19ee5ff64ff65457787e567b
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-22 13:21:00 -0700

    user notifications

---
+++ server/src/db/migrations/20250622201115_user_notifications.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('user_notifications', (table) => {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').index();
+    table.boolean('email');
+    table.boolean('sms');
+    table.boolean('push');
+    table.date('deleted_at');
+    table.date('created_at');
+    table.date('updated_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('user_notifications');
+} 
