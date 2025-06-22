ff8878aa2fe92565197abbd2c615ad2b05694c18
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-22 13:40:00 -0700

    user plants

---
+++ server/src/db/migrations/20250622203128_user_plants.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('user_plants', (table) => {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').index();
+    table.integer('plant_id').unsigned().references('id').inTable('plants').onDelete('CASCADE').index();
+    table.boolean('currently_growing');
+    table.boolean('grow_again');
+    table.date('deleted_at');
+    table.date('created_at');
+    table.date('updated_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('user_plants');
+}
