a1eacb7fa8b79afdc06612aca95c71bd616177c2
Wowsaruss <russell.hayes.dev@gmail.com>
2025-07-22 10:18:19 -0700

    Debug

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,22 +1,11 @@
 import { Slot } from 'expo-router';
-import { useAuth } from '../../context/AuthContext';
-import { Text, View } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
 
 export default function TabsLayout() {
-  const { accessToken } = useAuth();
-
-  if (!accessToken) {
-    return (
-      <View>
-        <Text>DEBUG: Tabs Layout Rendered (not authenticated)</Text>
-      </View>
-    );
-  }
-
   return (
-    <View>
-      <Text>DEBUG: Tabs Layout Rendered (authenticated)</Text>
+    <>
+      <ThemedText>DEBUG: Tabs Layout Rendered</ThemedText>
       <Slot />
-    </View>
+    </>
   );
 }
